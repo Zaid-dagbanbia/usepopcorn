b893d9dc991c45aadc115e5c03f733c13c8809fa
Zaid Dagbanbia <mohammed.dagbanbia@dci-student.org>
2024-05-12 06:53:20 +0200

    more details about useState

--- src/App-v1.jsx
+++ src/App-v1.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import StarRating from "./StarRating";
 
-/* const tempMovieData = [
+const tempMovieData = [
   {
     imdbID: "tt1375666",
     Title: "Inception",
@@ -46,7 +46,7 @@ const tempWatchedData = [
     imdbRating: 8.5,
     userRating: 9,
   },
-]; */
+];
 
 const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
